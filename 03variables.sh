710ca0be7f4aa7b70d7b51b1ee44b43319eb3b15
Aditya Mishra <139211017+AdityaMishra2327@users.noreply.github.com>
2024-09-07 17:01:34 +0530

    Add files via upload

---
+++ 03variables.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+
+
+#Variables
+
+name="Aditya"
+age=20
+
+
+
+echo "My name is $name and age is $age "
+
+name="Aditi"
+
+echo "My name is $name "
+
+# var to store the output of a command 
+
+
+HOSTNAME=$(hostname)
+
+echo "Name of this machine is $HOSTNAME "
+
+
+
+
+
+
+
+
+ 
